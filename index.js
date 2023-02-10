73528f341f902468f5a462c456a9f6e13a11be0a
cyd <ceydatoker2209@gmail.com>
2023-02-10 13:22:04 +0200

    responsiveness of todolist implemented

--- index.js
+++ index.js
@@ -11,21 +11,21 @@ function deleteButton(toDo) {
   return deleteToDoButton;
 }
 
-function createEditButton(newToDo, toDo) {
+function createEditButton(newToDo, toDo, buttons) {
   const editButton = document.createElement("button");
   editButton.classList.add("buttonStyling");
   editButton.innerText = "Edit";
   editButton.addEventListener("click", function () {
     const inputField = document.createElement("input");
     inputField.value = newToDo.innerText;
-    inputField.classList.add("inputField");
+    inputField.classList.add("inputField", "editField");
     toDo.replaceChild(inputField, newToDo);
     inputField.focus();
     inputField.addEventListener("blur", function () {
       newToDo.innerText = inputField.value;
       toDo.replaceChild(newToDo, inputField);
-      toDo.removeChild(editButton);
-      toDo.appendChild(createEditButton(newToDo));
+      buttons.removeChild(editButton);
+      butt.appendChild(createEditButton(newToDo, toDo, buttons));
     });
   });
   return editButton;
@@ -35,15 +35,22 @@ addButton.addEventListener("click", function () {
   if (inputField.value.trim() !== "") {
     const newToDo = document.createElement("p");
     newToDo.innerText = inputField.value;
-    const toDoItem = document.createElement("div");
-    toDoItem.classList.add("toDoItem");
-    const editButton = createEditButton(newToDo, toDoItem);
-    const deleteToDoButton = deleteButton(toDoItem);
-    toDoItem.appendChild(newToDo);
-    toDoItem.appendChild(deleteToDoButton);
-    toDoItem.appendChild(editButton);
+    const toDoItemContainer = document.createElement("div");
+    const buttonContainer = document.createElement("div");
+    buttonContainer.classList.add("modifyToDoButton");
+    toDoItemContainer.classList.add("toDoItem");
+    const editButton = createEditButton(
+      newToDo,
+      toDoItemContainer,
+      buttonContainer
+    );
+    const deleteToDoButton = deleteButton(toDoItemContainer);
+    buttonContainer.appendChild(deleteToDoButton);
+    buttonContainer.appendChild(editButton);
+    toDoItemContainer.appendChild(newToDo);
+    toDoItemContainer.appendChild(buttonContainer);
     newToDo.classList.add("paragraphStyling");
-    toDoContainer.appendChild(toDoItem);
+    toDoContainer.appendChild(toDoItemContainer);
     inputField.value = " ";
   }
 });
